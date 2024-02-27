1765b07bdbbe2e211d7623dcb02b794acc87fc38
A Kasarin <>
2024-02-27 13:49:45 +0100

    Init

---
+++ WorldOfPayback/WorldOfPayback/WorldOfPaybackApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPaybackApp.swift
+//  WorldOfPayback
+//
+//  Created by Anton Kasaryn on 27.02.24.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPaybackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
